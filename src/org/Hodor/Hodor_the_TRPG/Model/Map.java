a7b1eea2fd64e010935a5bae7fd24701c4d3f199
Joshua Koike <jkoike2013@gmail.com>
2014-11-05 18:05:10 -0700

    TERRAIN GENERATION, MOFOS.

---
+++ src/org/Hodor/Hodor_the_TRPG/Model/Map.java
@@ -0,0 +1,11 @@
+package org.Hodor.Hodor_the_TRPG.Model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jkoike on 11/5/14.
+ */
+public class Map {
+    private Tile[][] map;
+    private ArrayList<Unit> units;
+}
---
+++ src/org/Hodor/Hodor_the_TRPG/Util/MapGenerator.java
@@ -0,0 +1,64 @@
+package org.Hodor.Hodor_the_TRPG.Util;
+
+import java.util.Random;
+
+/**
+ * Created by jkoike on 11/5/14.
+ */
+public class MapGenerator {
+    // 2d array representing the map. (0,0) is the top left corner. Coordinates are in (x, y),
+    //      where y is the vertical axis.
+    int[][] map;
+    Random random;
+    public MapGenerator(int size){
+        map = new int[size][size];
+        random = new Random();
+    }
+
+    private synchronized void linearInterpolate(int topX, int topY, int h, int w, float var){
+        if(h < 2 || w < 2) return;
+        int tL = map[topX][topY];
+        int bL = map[topX][topY + h];
+        int tR = map[topX + w][topY];
+        int bR = map[topX + w][topY + h];
+        int variance;
+        {
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+            for(int i : new int[]{tL, bL, tR, bR}){
+                max = (i > max)?i:max;
+                min = (i < min)?i:min;
+            }
+            variance = (int)Math.abs((max - min) * var);
+            variance = (variance == 0)?1:variance;
+            System.out.println(variance);
+        }
+        map[topX][topY + (h/2)] = (tL + bL)/2 + (random.nextInt(variance*2)-variance);
+        map[topX + (w/2)][topY] = (tL + tR)/2 + (random.nextInt(variance*2)-variance);
+        map[topX + (w/2)][topY + (h/2)] = (tR + bR + bL + tL)/4 + (random.nextInt(variance*2)-variance);
+        map[topX + w][topY + (h/2)] = (tR + bR)/2 + (random.nextInt(variance*2)-variance);
+        map[topX + (w/2)][topY + h] = (bR + bL)/2 + (random.nextInt(variance*2)-variance);
+        linearInterpolate(topX, topY, h/2, w/2, var); // Top left corner
+        linearInterpolate(topX + (w/2), topY, h/2, w/2, var); // Top right corner
+        linearInterpolate(topX, topY + (h/2), h/2, w/2, var); // Bottom left corner
+        linearInterpolate(topX + (w/2), topY + (h/2), h/2, w/2, var); //Bottom right corner
+    }
+
+    public int[][] generate() {
+        map[0][0] = random.nextInt(20);
+        map[map.length-1][0] = random.nextInt(20);
+        map[0][map[0].length-1] = random.nextInt(20);
+        map[map.length-1][map[0].length-1] = random.nextInt(20);
+        linearInterpolate(0,0,map[0].length - 1, map.length - 1, .5f);
+        return map;
+    }
+
+    public static void main(String[] args) {
+        for(int[] row : new MapGenerator(17).generate()){
+            for(int i : row){
+                System.out.printf("%5d", i);
+            }
+            System.out.println();
+        }
+    }
+}
