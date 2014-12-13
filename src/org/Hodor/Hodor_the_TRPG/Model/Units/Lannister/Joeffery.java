08c9394ac2228ddb84ce385b5fcde906daca7d59
Jason Tom <jtom77@gmail.com>
2014-12-12 20:01:49 -0700

    Adding Image in Units

--- src/org/Hodor/Hodor_the_TRPG/Model/Units/Lannister/Joeffery.java
+++ src/org/Hodor/Hodor_the_TRPG/Model/Units/Lannister/Joeffery.java
@@ -1,7 +1,9 @@
 package org.Hodor.Hodor_the_TRPG.Model.Units.Lannister;
 
+import org.Hodor.Hodor_the_TRPG.Delegate;
 import org.Hodor.Hodor_the_TRPG.Model.House;
 import org.Hodor.Hodor_the_TRPG.Model.Units.Archer;
+import org.Hodor.Hodor_the_TRPG.R;
 
 /**
  * Created by jkoike on 12/2/14.
@@ -10,5 +12,6 @@ public class Joeffery extends Archer {
     public Joeffery(int x, int y){
         super(x, y, "Joeffery", House.Lannister, 50, 20, 20, 0, 7, 4);
         maxZ = 3;
+        this.image = Delegate.getMapView().getResources().getDrawable(R.drawable.lannisters_archer);
     }
 }
--- src/org/Hodor/Hodor_the_TRPG/Model/Units/Lannister/Tyrion.java
+++ src/org/Hodor/Hodor_the_TRPG/Model/Units/Lannister/Tyrion.java
@@ -1,7 +1,9 @@
 package org.Hodor.Hodor_the_TRPG.Model.Units.Lannister;
 
+import org.Hodor.Hodor_the_TRPG.Delegate;
 import org.Hodor.Hodor_the_TRPG.Model.House;
 import org.Hodor.Hodor_the_TRPG.Model.Units.Rogue;
+import org.Hodor.Hodor_the_TRPG.R;
 
 /**
  * Created by jkoike on 12/2/14.
@@ -9,5 +11,6 @@ import org.Hodor.Hodor_the_TRPG.Model.Units.Rogue;
 public class Tyrion extends Rogue {
     public Tyrion(int x, int y){
         super(x, y, "Tyrion", House.Lannister, 105, 18, 16, 10, 6, 1);
+        this.image = Delegate.getMapView().getResources().getDrawable(R.drawable.lannisters_rogue);
     }
 }
